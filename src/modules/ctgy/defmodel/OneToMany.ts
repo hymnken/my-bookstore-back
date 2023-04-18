703b02cda8c85ff560041a013ef9d69c54283785
kennyouchou <kennyouchou@gmail.com>
2023-04-19 00:30:23 +0800

    多表关联查询

--- src/modules/ctgy/defmodel/OneToMany.ts
+++ src/modules/ctgy/defmodel/OneToMany.ts
@@ -1,3 +1,34 @@
 import { secondCtgyModel } from './SecCtgyModel'
 import { thirdCtgyModel } from './ThirdCtgyModel'
 
+// OneToMany
+secondCtgyModel.hasMany(thirdCtgyModel, {
+  as: 'thirdctgy',
+  foreignKey: 'secctgyid',
+})
+
+//ManyToOne
+
+thirdCtgyModel.belongsTo(secondCtgyModel, {
+  foreignKey: 'secctgyid',
+  targetKey: 'secondctgyid', //主键
+})
+
+function findSecThrdCtgysByFstCtgyId(firstctgyId: number) {
+    const result = secondCtgyModel.findAll({
+        raw: true,
+        where: {
+            firstctgyId
+        },
+        include: [
+            {
+                model: thirdCtgyModel,
+                as: 'thirdctgy',
+            }
+        ]
+    })
+    console.log(result);
+    
+}
+
+findSecThrdCtgysByFstCtgyId(1)
